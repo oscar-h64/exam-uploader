ba208e5943b73976b614bc303c84dad56a11af2d
Oscar Harris <oscar@oscar-h.com>
2021-04-03 15:32:53 +0100

    Finish AEP support

--- app/Main.hs
+++ app/Main.hs
@@ -12,17 +12,21 @@ import Data.UUID ( UUID, fromString )
 
 import Options.Applicative
 
+import Servant.Client.Core ( parseBaseUrl )
+
+import System.Exit
 import System.FilePath ( splitFileName, takeFileName )
 import System.FSNotify
 
-import Lib
+import Warwick.AEP
 
 --------------------------------------------------------------------------------
 
 data Options = MkOptions {
     optSSC :: Text,
-    optAssessment :: UUID,
-    optFile :: FilePath
+    optMAssessment :: Maybe UUID,
+    optFile :: FilePath,
+    optMInstance :: Maybe AEPInstance
 }
 
 options :: Parser Options
@@ -30,7 +34,7 @@ options = MkOptions <$> strOption (  long "ssc"
                                   <> metavar "SSC"
                                   <> help "The value of your AEP SSC cookie"
                                   )
-                    <*> fmap (fromJust . fromString) (strOption $
+                    <*> fmap fromString (strOption
                             (  long "assessment"
                             <> metavar "ASSESSMENT"
                             <> help "The UUID of the assessment in AEP"
@@ -39,6 +43,13 @@ options = MkOptions <$> strOption (  long "ssc"
                                   <> metavar "FILEPATH"
                                   <> help "The file to watch and upload"
                                   )
+                    <*> fmap instanceFromString (strOption
+                            (  long "instance"
+                            <> metavar "live|sandbox|URL"
+                            <> help "The AEP instance to use"
+                            <> value "live"
+                            <> showDefault
+                            ))
 
 optInfo :: ParserInfo Options
 optInfo = info (options <**> helper) 
@@ -48,17 +59,40 @@ optInfo = info (options <**> helper)
 
 --------------------------------------------------------------------------------
 
+instanceFromString :: String -> Maybe AEPInstance
+instanceFromString "live" = pure Live
+instanceFromString "sandbox" = pure Sandbox
+instanceFromString url = CustomInstance <$> parseBaseUrl url
+
+--------------------------------------------------------------------------------
+
 main :: IO ()
 main = do
     MkOptions{..} <- execParser optInfo
+
+    optAssessment <- case optMAssessment of
+        Nothing -> putStrLn "Invalid UUID for assessment" >> exitFailure
+        Just uuid -> pure uuid
+
+    optInstance <- case optMInstance of
+        Nothing -> putStrLn "Invalid AEP instance" >> exitFailure
+        Just inst -> pure inst
 
     let (dir, file) = splitFileName optFile
 
     let pred (Modified path _ False) = takeFileName path == file
         pred _ = False
 
+    let action event = do
+            let path = eventPath event
+            res <- withAEP optInstance optSSC
+                 $ uploadFile optAssessment path True
+            
+            -- Print if there was an error making the request
+            either print (const $ putStrLn "File Uploaded Successfully") res
+
     withManager $ \mgr -> do
-        watchDir mgr dir pred print
+        watchDir mgr dir pred action
 
         putStrLn "Watcher started. Press enter to stop"
         void $ getLine
